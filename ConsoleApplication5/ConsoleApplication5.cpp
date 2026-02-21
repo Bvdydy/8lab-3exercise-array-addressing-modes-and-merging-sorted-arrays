80894cbdcf33fe7dd0dc15df776f4381608a4046
New <New@KOMPUTER>
2026-02-21 18:46:06 +0700

    Слияние массивов с обоими типами адресации (финальная версия)"

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -1,45 +1,68 @@
-﻿// exercise3_v3.cpp
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
-#define SIZE 10
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    // Вариант 3: Динамический + Индексная
-    cout << "\n=== Вариант 3: Динамический + Индексная ===" << endl;
-    int* dynamicArray3 = new int[SIZE];
+    int size1 = 7;
+    int size2 = 6;
 
-    for (int i = 0; i < SIZE; i++) {
-        dynamicArray3[i] = i * i;
+    int* arr1 = new int[size1] {1, 5, 8, 12, 15, 20, 25};
+    int* arr2 = new int[size2] {3, 7, 9, 14, 18, 22};
+
+    int size3 = size1 + size2;
+    int* mergedArray = new int[size3];
+
+    cout << "=== Объединение упорядоченных массивов ===" << endl;
+    cout << "Массив 1: ";
+    for (int i = 0; i < size1; i++) cout << arr1[i] << " ";
+    cout << endl;
+
+    cout << "Массив 2: ";
+    for (int* p = arr2, i = 0; i < size2; i++, p++) cout << *p << " ";
+    cout << endl;
+
+    // Слияние с индексной адресацией
+    int i = 0, j = 0, k = 0;
+
+    while (i < size1 && j < size2) {
+        if (arr1[i] <= arr2[j]) {
+            mergedArray[k++] = arr1[i++];
+        }
+        else {
+            mergedArray[k++] = arr2[j++];
+        }
     }
 
-    cout << "Заполнение: ";
-    for (int i = 0; i < SIZE; i++) {
-        cout << dynamicArray3[i] << " ";
+    while (i < size1) mergedArray[k++] = arr1[i++];
+
+    // Копирование остатка с указательной адресацией
+    int* ptr2 = arr2 + j;
+    while (j < size2) {
+        mergedArray[k++] = *ptr2;
+        ptr2++;
+        j++;
+    }
+
+    // Вывод с индексной адресацией
+    cout << "\nОбъединённый (индекс): ";
+    for (int idx = 0; idx < size3; idx++) {
+        cout << mergedArray[idx] << " ";
     }
     cout << endl;
 
-    delete[] dynamicArray3;
-
-    // Вариант 4: Динамический + Указательная
-    cout << "\n=== Вариант 4: Динамический + Указатель ===" << endl;
-    int* dynamicArray4 = new int[SIZE];
-
-    for (int* ptr = dynamicArray4, i = 0; i < SIZE; i++, ptr++) {
-        *ptr = i * i;
-    }
-
-    cout << "Заполнение: ";
-    for (int* ptr = dynamicArray4, i = 0; i < SIZE; i++, ptr++) {
+    // Вывод с указательной адресацией
+    cout << "Объединённый (указатель): ";
+    for (int* ptr = mergedArray, idx = 0; idx < size3; idx++, ptr++) {
         cout << *ptr << " ";
     }
     cout << endl;
 
-    delete[] dynamicArray4;
+    delete[] arr1;
+    delete[] arr2;
+    delete[] mergedArray;
 
     return 0;
 }
